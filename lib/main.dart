a2941406e84786a8d83073a127cfd45cf7ec90e3
tomikartemik <artemules@mail.ru>
2022-03-01 10:01:26 +0300

    UI completed

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:invise_flutter/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
